853e0fd7174a21af157a4c48b27e7a5eb52968ad
--local <breakmodejason@gmail.com>
2026-03-02 17:13:55 +1100

    chaneg teh analyse from next to react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "tailwindcss";
 import CheckinPage from './CheckinPage1';
 import CheckinPage2 from './CheckinPage2';
